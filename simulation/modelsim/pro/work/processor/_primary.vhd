afb9d9b87f350a29db03891448cb3a4125f348ec
Vishrantt <vishranttripathi@gmail.com>
2015-11-21 20:58:47 +0530

    only verilog files

--- simulation/modelsim/pro/work/processor/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity processor is
-    port(
-        clk             : in     vl_logic;
-        reset_pin       : in     vl_logic
-    );
-end processor;
